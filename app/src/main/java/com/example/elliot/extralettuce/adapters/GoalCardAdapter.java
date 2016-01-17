c8038f80c8552f68d1fd17ff202c22e42f0d7d7b
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-01-16 20:06:46 -0800

    Updated GoalCardAdaptor

--- app/src/main/java/com/example/elliot/extralettuce/adapters/GoalCardAdapter.java
+++ app/src/main/java/com/example/elliot/extralettuce/adapters/GoalCardAdapter.java
@@ -1,8 +1,11 @@
 package com.example.elliot.extralettuce.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.elliot.extralettuce.dataClasses.Goal;
 
 import java.util.List;
 
@@ -10,10 +13,10 @@ import java.util.List;
  * Created by Matt on 1/16/2016.
  */
 public class GoalCardAdapter extends RecyclerView.Adapter<GoalCardAdapter.ViewHolder> {
-    private List<>
+    private List<Goal> goalList;
 
-    public GoalCardAdapter(List<>) {
-
+    public GoalCardAdapter(List<Goal> goals) {
+        goalList = goals;
     }
 
     @Override
@@ -28,14 +31,29 @@ public class GoalCardAdapter extends RecyclerView.Adapter<GoalCardAdapter.ViewHo
 
     @Override
     public int getItemCount() {
-        return 0;
+        return goalList != null ? goalList.size() : 0;
     }
 
-    private class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder {
         public ViewHolder(View itemView) {
             super(itemView);
         }
     }
 
+    public void addGoal(Goal goal) {
+        goalList.add(goal);
+        sortList();
+        notifyDataSetChanged();
+    }
+
+    public void removeGoal(int pos) {
+        goalList.remove(pos);
+        notifyDataSetChanged();
+    }
+
+
+    private void sortList() {
+
+    }
 
 }
