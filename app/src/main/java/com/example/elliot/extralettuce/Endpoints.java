8041c8a9db5e6073b97f5a7aa2c900534acb2481
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-01-16 21:54:48 -0800

    "Sir Commits A Lot" - Volume 6

---
+++ app/src/main/java/com/example/elliot/extralettuce/Endpoints.java
@@ -0,0 +1,9 @@
+package com.example.elliot.extralettuce;
+
+/**
+ * Created by Elliot on 1/16/16.
+ */
+public class Endpoints {
+    public static final String BASE_URL = "www.extralettuce/co/account";
+    public static final String GOALS = "goals";
+}
