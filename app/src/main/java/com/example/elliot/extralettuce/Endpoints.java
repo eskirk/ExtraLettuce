90664bbb8e75f4291f6f83c0b7a2f174a1f2c7d2
Boris Tam <boris22t@gmail.com>
2016-01-17 12:29:29 -0800

    BUg fix

--- app/src/main/java/com/example/elliot/extralettuce/Endpoints.java
+++ app/src/main/java/com/example/elliot/extralettuce/Endpoints.java
@@ -4,6 +4,6 @@ package com.example.elliot.extralettuce;
  * Created by Elliot on 1/16/16.
  */
 public class Endpoints {
-    public static final String BASE_URL = "www.extralettuce/co/account";
+    public static final String BASE_URL = "http://www.extralettuce.co/account";
     public static final String GOALS = "goals";
 }
