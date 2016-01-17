b01aa98a76fda5998bf01b7d40b14462429395e7
MattS8 <Matthew.steinhardt@gmail.com>
2016-01-16 19:49:21 -0800

    GoalCardAdapter
    Added GoalCardAdapter.

---
+++ app/src/main/java/com/example/elliot/extralettuce/adapters/GoalCardAdapter.java
@@ -0,0 +1,41 @@
+package com.example.elliot.extralettuce.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Matt on 1/16/2016.
+ */
+public class GoalCardAdapter extends RecyclerView.Adapter<GoalCardAdapter.ViewHolder> {
+    private List<>
+
+    public GoalCardAdapter(List<>) {
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    private class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
+}
