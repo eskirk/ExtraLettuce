b11b020978fee582c466dc7745062d7fa37fad35
MattS8 <Matthew.steinhardt@gmail.com>
2016-01-17 02:31:09 -0800

    Some Fixes

--- app/src/main/java/com/example/elliot/extralettuce/MainActivity.java
+++ app/src/main/java/com/example/elliot/extralettuce/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
