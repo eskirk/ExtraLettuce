8168accfad9a61fd66801c7f75ae1a3e97ecf9b3
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-01-17 16:52:53 -0800

    "Sir Commits A Lot" - Volume 10

--- app/src/main/java/com/example/elliot/extralettuce/IntroActivity.java
+++ app/src/main/java/com/example/elliot/extralettuce/IntroActivity.java
@@ -1,29 +1,19 @@
 package com.example.elliot.extralettuce;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.example.elliot.extralettuce.support.Typefaces;
 
 public class IntroActivity extends AppCompatActivity {
 
-    TextView screenTitle;
     Button login, getStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        screenTitle = (TextView) findViewById(R.id.introScreenTitle);
-        screenTitle.setTypeface(Typefaces.yeahPapa(this));
         login = (Button) findViewById(R.id.login);
         getStarted = (Button) findViewById(R.id.get_started);
         login.setOnClickListener(new View.OnClickListener() {
